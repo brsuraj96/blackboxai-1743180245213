8cf00070cf13d3f5172b695222eb45d437c6e87d
brsuraj96 <brsuraj96@gmail.com>
2025-03-28 22:30:32 +0530

    Add PuzzleGame/App.tsx 1743181231682

---
+++ PuzzleGame/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import EnhancedPuzzleGame from './EnhancedPuzzleGame';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <EnhancedPuzzleGame />
+    </>
+  );
+}
